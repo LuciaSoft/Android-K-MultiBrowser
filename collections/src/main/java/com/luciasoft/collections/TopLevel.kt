14d3b1ed7dfcb238e908bf1722785dcd38b4ecdd
LuciaSoft <info.luciasoft@gmail.com>
2023-12-04 19:49:16 -0500

    update collections/src/main/java/com/luciasoft/collections/TopLevel.kt

--- collections/src/main/java/com/luciasoft/collections/TopLevel.kt
+++ collections/src/main/java/com/luciasoft/collections/TopLevel.kt
@@ -7,9 +7,7 @@ fun<T> randomize(data: Collection<T>, seed: Int = 0): ArrayList<T>
 {
     val r = Random(seed)
     val len = data.size
-    val list =
-        if (data is ArrayList<T>) data as ArrayList<T>
-        else ArrayList<T>(data)
+    val list = data as? ArrayList<T> ?: ArrayList<T>(data)
     for (i in 0 until len)
     {
         val pos = (r.nextDouble() * len).toInt()
