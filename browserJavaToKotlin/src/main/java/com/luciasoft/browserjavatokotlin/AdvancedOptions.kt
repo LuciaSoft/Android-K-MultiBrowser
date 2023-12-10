f8de7000ec427e1490be93ceb038627fd111bd18
LuciaSoft <info.luciasoft@gmail.com>
2023-12-09 23:37:56 -0500

    update browserJavaToKotlin/src/main/java/com/luciasoft/browserjavatokotlin/AdvancedOptions.kt

--- browserJavaToKotlin/src/main/java/com/luciasoft/browserjavatokotlin/AdvancedOptions.kt
+++ browserJavaToKotlin/src/main/java/com/luciasoft/browserjavatokotlin/AdvancedOptions.kt
@@ -39,9 +39,9 @@ class AdvancedOptions
     }
 
     var debugMode = false
-    var screenRotationMode: MultiBrowserOptions.ScreenMode? = null
-    var shortClickSaveFileBehavior: MultiBrowserOptions.SaveFileBehavior? = null
-    var longClickSaveFileBehavior: MultiBrowserOptions.SaveFileBehavior? = null
+    lateinit var screenRotationMode: MultiBrowserOptions.ScreenMode
+    lateinit var shortClickSaveFileBehavior: MultiBrowserOptions.SaveFileBehavior
+    lateinit var longClickSaveFileBehavior: MultiBrowserOptions.SaveFileBehavior
     var allowShortClickFileForLoad = false
     var allowShortClickFileForSave = false
     var allowLongClickFileForLoad = false
@@ -72,5 +72,5 @@ class AdvancedOptions
 
     var autoRefreshDirectorySource = false
 
-    var mediaStoreImageExts: String? = null
+    lateinit var mediaStoreImageExts: String
 }
