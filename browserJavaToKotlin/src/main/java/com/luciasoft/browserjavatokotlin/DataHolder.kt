8cd0cf73bcbedfcb223aa78447efb03b9ebaacd9
LuciaSoft <info.luciasoft@gmail.com>
2023-12-10 23:51:46 -0500

    update browserJavaToKotlin/src/main/java/com/luciasoft/browserjavatokotlin/DataHolder.kt

--- browserJavaToKotlin/src/main/java/com/luciasoft/browserjavatokotlin/DataHolder.kt
+++ browserJavaToKotlin/src/main/java/com/luciasoft/browserjavatokotlin/DataHolder.kt
@@ -8,21 +8,13 @@ import com.luciasoft.collections.MediaStoreImageInfoTree
 class DataHolder(var app: Application)
     : AndroidViewModel(app)
 {
-    @JvmField
     var OPT: Options? = null
-    @JvmField
     var ADV: AdvancedOptions? = null
-    @JvmField
     var THM: ThemeOptions? = null
-    @JvmField
     var mDefaultScreenOrientation: Int? = null
-    @JvmField
     var mFileSystemDirectoryItems: ArrayList<DirectoryItem>? = null
-    @JvmField
     var mMediaStoreImageInfoList: ArrayList<DirectoryItem>? = null
-    @JvmField
     var mMediaStoreImageInfoTree: MediaStoreImageInfoTree? = null
-    @JvmField
     var mFirstLoad = true
 
     init
