5f309d1b00636eac82a96018952c2b8a45cabc34
LuciaSoft <info.luciasoft@gmail.com>
2023-12-12 16:59:45 -0500

    XmlOps

--- browserJavaToKotlin/src/main/java/com/luciasoft/browserjavatokotlin/XmlOperations.kt
+++ browserJavaToKotlin/src/main/java/com/luciasoft/browserjavatokotlin/XmlOperations.kt
@@ -44,15 +44,22 @@ internal object XmlOperations
                 val typeStr = getTypeStr(info.type)
                 val typeStrLower = typeStr.lowercase()
                 if (typeStrLower.startsWith("array")) continue
-                val valStr = when (typeStrLower)
-                {
-                    "char" -> "" + (info.value as Char).code
-                    "char?" -> "" + (info.value as Char?)?.code
-                    else -> "" + info.value
-                }
                 val element = doc.createElement("$head.${info.name}")
                 element.setAttribute("type", typeStr)
-                element.setAttribute("value", valStr)
+                val value = info.value
+                if (value == null)
+                {
+                    element.setAttribute("is_null", "" + true)
+                }
+                else
+                {
+                    val valStr = when (typeStrLower)
+                    {
+                        "char", "char?" -> "" + (value as Char).code
+                        else -> "" + value
+                    }
+                    element.setAttribute("value", valStr)
+                }
                 root.appendChild(element)
             }
         }
@@ -79,10 +86,14 @@ internal object XmlOperations
             for (info in tree)
             {
                 val element = try { root.getElementsByTagName("$head.${info.name}").item(0) as Element } catch (ex: Exception) { continue }
-                val type = element.getAttribute("type").lowercase()
-                val valStr = element.getAttribute("value")
-
-                if (setProperty(type, valStr, info)) count++
+                val typeStr = element.getAttribute("type").lowercase()
+                val valStr = if (element.hasAttribute("value")) element.getAttribute("value") else null
+                if (valStr == null)
+                {
+                    if (!element.hasAttribute("is_null")
+                        || element.getAttribute("is_null") != "" + true) throw Exception()
+                }
+                if (setProperty(typeStr, valStr, info)) count++
                 else skipped++
             }
         }
@@ -90,174 +101,156 @@ internal object XmlOperations
         return arrayOf(count, skipped)
     }
 
-    private fun setProperty(type: String, valStr: String, info: PropertyInfo): Boolean
+    private fun setProperty(type: String, valStr: String?, info: PropertyInfo): Boolean
     {
         when (type)
         {
             "char" -> {
-                val value = valStr.toInt()
                 val prop = info.prop as KMutableProperty1<Any, Char>
-                prop.set(info.instance, value.toChar())
+                prop.set(info.instance, valStr!!.toInt().toChar())
                 return true
             }
 
             "char?" -> {
-                val value = if (valStr == "null") null else valStr.toInt()
                 val prop = info.prop as KMutableProperty1<Any, Char?>
-                prop.set(info.instance, value?.toChar())
+                prop.set(info.instance, valStr?.toInt()?.toChar())
                 return true
             }
 
             "byte" -> {
-                val value = valStr.toByte()
                 val prop = info.prop as KMutableProperty1<Any, Byte>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr!!.toByte())
                 return true
             }
 
             "byte?" -> {
-                val value = if (valStr == "null") null else valStr.toByte()
                 val prop = info.prop as KMutableProperty1<Any, Byte?>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr?.toByte())
                 return true
             }
 
             "short" -> {
-                val value = valStr.toShort()
                 val prop = info.prop as KMutableProperty1<Any, Short>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr!!.toShort())
                 return true
             }
 
             "short?" -> {
-                val value = if (valStr == "null") null else valStr.toShort()
                 val prop = info.prop as KMutableProperty1<Any, Short?>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr?.toShort())
                 return true
             }
 
             "int" -> {
-                val value = valStr.toInt()
                 val prop = info.prop as KMutableProperty1<Any, Int>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr!!.toInt())
                 return true
             }
 
             "int?" -> {
-                val value = if (valStr == "null") null else valStr.toInt()
                 val prop = info.prop as KMutableProperty1<Any, Int?>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr?.toInt())
                 return true
             }
 
             "long" -> {
-                val value = valStr.toLong()
                 val prop = info.prop as KMutableProperty1<Any, Long>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr!!.toLong())
                 return true
             }
 
             "long?" -> {
-                val value = if (valStr == "null") null else valStr.toLong()
                 val prop = info.prop as KMutableProperty1<Any, Long?>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr?.toLong())
                 return true
             }
 
             "float" -> {
-                val value = valStr.toFloat()
                 val prop = info.prop as KMutableProperty1<Any, Float>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr!!.toFloat())
                 return true
             }
 
             "float?" -> {
-                val value = if (valStr == "null") null else valStr.toFloat()
                 val prop = info.prop as KMutableProperty1<Any, Float?>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr?.toFloat())
                 return true
             }
 
             "double" -> {
-                val value = valStr.toDouble()
                 val prop = info.prop as KMutableProperty1<Any, Double>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr!!.toDouble())
                 return true
             }
 
             "double?" -> {
-                val value = if (valStr == "null") null else valStr.toDouble()
                 val prop = info.prop as KMutableProperty1<Any, Double?>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr?.toDouble())
                 return true
             }
 
             "boolean" -> {
-                val value = valStr.toBoolean()
                 val prop = info.prop as KMutableProperty1<Any, Boolean>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr!!.toBoolean())
                 return true
             }
 
             "boolean?" -> {
-                val value = if (valStr == "null") null else valStr.toBoolean()
                 val prop = info.prop as KMutableProperty1<Any, Boolean?>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr?.toBoolean())
                 return true
             }
 
             "string" -> {
-                val value = valStr
                 val prop = info.prop as KMutableProperty1<Any, String>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr!!)
                 return true
             }
 
             "string?" -> {
-                val value = if (valStr == "null") null else valStr
                 val prop = info.prop as KMutableProperty1<Any, String?>
-                prop.set(info.instance, value)
+                prop.set(info.instance, valStr)
                 return true
             }
 
             "browsemode" -> {
-                val value = BrowseMode.valueOf(getNumFromVal(valStr))
                 val prop = info.prop as KMutableProperty1<Any, BrowseMode>
+                val value = BrowseMode.valueOf(getNumFromVal(valStr!!))
                 prop.set(info.instance, value)
                 return true
             }
 
             "browserviewtype" -> {
-                val value = BrowserViewType.valueOf(getNumFromVal(valStr))
                 val prop = info.prop as KMutableProperty1<Any, BrowserViewType>
+                val value = BrowserViewType.valueOf(getNumFromVal(valStr!!))
                 prop.set(info.instance, value)
                 return true
             }
 
             "sortorder" -> {
-                val value = SortOrder.valueOf(getNumFromVal(valStr))
                 val prop = info.prop as KMutableProperty1<Any, SortOrder>
+                val value = SortOrder.valueOf(getNumFromVal(valStr!!))
                 prop.set(info.instance, value)
                 return true
             }
 
             "savefilebehavior" -> {
-                val value = SaveFileBehavior.valueOf(getNumFromVal(valStr))
                 val prop = info.prop as KMutableProperty1<Any, SaveFileBehavior>
+                val value = SaveFileBehavior.valueOf(getNumFromVal(valStr!!))
                 prop.set(info.instance, value)
                 return true
             }
 
             "screenmode" -> {
-                val value = ScreenMode.valueOf(getNumFromVal(valStr))
                 val prop = info.prop as KMutableProperty1<Any, ScreenMode>
+                val value = ScreenMode.valueOf(getNumFromVal(valStr!!))
                 prop.set(info.instance, value)
                 return true
             }
 
             "fontmode" -> {
-                val value = FontMode.valueOf(getNumFromVal(valStr))
                 val prop = info.prop as KMutableProperty1<Any, FontMode>
+                val value = FontMode.valueOf(getNumFromVal(valStr!!))
                 prop.set(info.instance, value)
                 return true
             }
