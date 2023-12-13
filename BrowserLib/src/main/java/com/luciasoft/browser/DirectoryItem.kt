e29f3cf542f4de082510a8a2c776fcd483fede9a
LuciaSoft <info.luciasoft@gmail.com>
2023-12-13 18:02:31 -0500

    update BrowserLib/src/main/java/com/luciasoft/browser/DirectoryItem.kt

--- BrowserLib/src/main/java/com/luciasoft/browser/DirectoryItem.kt
+++ BrowserLib/src/main/java/com/luciasoft/browser/DirectoryItem.kt
@@ -3,16 +3,16 @@ package com.luciasoft.browser
 import com.luciasoft.utils.getShortName
 
 // OPTIMIZED
-abstract class DirectoryItem(val path: String, var date: Long?, var info: String?)
+internal abstract class DirectoryItem(val path: String, var date: Long?, var info: String?)
 {
     val name = getShortName(path)
 }
 
 // OPTIMIZED
-class FileItem(path: String, date: Long?, var size: Long?, info: String?, var imageId: Int?)
+internal class FileItem(path: String, date: Long?, var size: Long?, info: String?, var imageId: Int?)
     : DirectoryItem(path, date, info)
 
 // OPTIMIZED
-class FolderItem(path: String, date: Long?, info: String?)
+internal class FolderItem(path: String, date: Long?, info: String?)
     : DirectoryItem(path, date, info)
 
